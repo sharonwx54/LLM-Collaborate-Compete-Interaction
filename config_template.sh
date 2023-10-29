53978f4ed5ec43b5383e2c0a01fd7d1290fc68de
Jensen Huang <jensenhuang2@gmail.com>
2023-10-29 01:58:04 -0400

    remove credentials

--- config_template.sh
+++ config_template.sh
@@ -1,5 +1,4 @@
 export USE_AZURE=False
-# export OPENAI_API_KEY=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
-export OPENAI_API_KEY=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
+export OPENAI_API_KEY=
 # export API_BASE=your-base-url-for-azure
 # export API_VERSION=2023-03-15-preview
