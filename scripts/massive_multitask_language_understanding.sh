bafa47e53f684b482f06cb06d4187aad371fff97
Jensen Huang <jensenhuang2@gmail.com>
2023-10-29 19:56:15 -0400

    update dataset sampling for mmlu

--- scripts/massive_multitask_language_understanding.sh
+++ scripts/massive_multitask_language_understanding.sh
@@ -5,7 +5,7 @@ MODEL="gpt-3.5-turbo"
 DATA_FILE="massive_multitask_language_understanding.jsonl"
 
 START_IDX=0
-END_IDX=1
+END_IDX=200
 
 # choose method
 METHOD="spp" # ['standard','cot','spp', 'spp_profile', 'spp_fixed_persona']
